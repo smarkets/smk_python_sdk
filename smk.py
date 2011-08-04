d628fc558a739808bce0791b68415623515c93d2
Hunter Morris <hunter.morris@smarkets.com>
2011-08-04 19:19:01 +0100

    Update client with latest definitions

--- smk.py
+++ smk.py
@@ -4,6 +4,12 @@ import struct
 from Queue import Queue
 from threading import Thread
 
+
+class DecodeError(Exception):
+    "Error encountered decoding a frame"
+    pass
+
+
 class Client(Thread):
 
     class Out(Thread):
@@ -12,7 +18,7 @@ class Client(Thread):
             self.seq = seq
             self.q = Queue(0)
             Thread.__init__(self)
-        
+
         def send(self, msg):
             self.q.put(msg)
             # todo: for persistence this should also log to a file
@@ -28,14 +34,13 @@ class Client(Thread):
                 header = None
 
                 msg = self.q.get(True)
-                msg.seq = self.seq
+                msg.sequenced.seq = self.seq
                 msg_bytes = msg.SerializeToString()
                 byte_count = len(msg_bytes)
-                # 0:1 Bytes:15 Data:(Bytes*8)
-                header = struct.pack('>H', byte_count)
-                # header already has 0 as first bit if byte_count < what 15 bits can store
-                # a 1 would mean it's a non-sequenced message
-                self.sock.send(header + msg_bytes)
+                # Pad to 4 bytes
+                padding = '\x00' * max(0, 3 - byte_count)
+                frame = _encode_varint(byte_count) + msg_bytes + padding
+                self.sock.send(frame)
                 self.q.task_done()
                 self.seq += 1
 
@@ -43,32 +48,32 @@ class Client(Thread):
         Thread.__init__(self)
 
     def order(self, qty, price, side, group, contract):
-        msg = seto_pb2.seq_message()
-        msg.payload.order_create.quantity = qty
-        msg.payload.order_create.price = price
-        msg.payload.order_create.side = side
-        msg.payload.order_create.group = group
-        msg.payload.order_create.contract = contract
+        msg = seto_pb2.payload()
+        msg.sequenced.message_data.order_create.quantity = qty
+        msg.sequenced.message_data.order_create.price = price
+        msg.sequenced.message_data.order_create.side = side
+        msg.sequenced.message_data.order_create.group = group
+        msg.sequenced.message_data.order_create.contract = contract
         self.out.send(msg)
 
     def order_cancel(self, order):
-        msg = seto_pb2.seq_message()
-        msg.payload.order_cancel.order = order
+        msg = seto_pb2.payload()
+        msg.sequenced.message_data.order_cancel.order = order
         self.out.send(msg)
 
     def ping(self):
-        msg = seto_pb2.seq_message()
-        msg.payload.ping = True
+        msg = seto_pb2.payload()
+        msg.sequenced.message_data.ping = True
         self.out.send(msg)
 
     def subscribe(self, group):
-        msg = seto_pb2.seq_message()
-        msg.payload.market_subscription.group = group
+        msg = seto_pb2.payload()
+        msg.sequenced.message_data.market_subscription.group = group
         self.out.send(msg)
 
     def unsubscribe(self, group):
-        msg = seto_pb2.seq_message()
-        msg.payload.market_unsubscription.group = group
+        msg = seto_pb2.payload()
+        msg.message.market_unsubscription.group = group
         self.out.send(msg)
 
     def login(self, host, port, username, password, session=None, inseq=1, outseq=1):
@@ -77,72 +82,100 @@ class Client(Thread):
         self.out = Client.Out(sock, outseq)
         self.out.start()
         self.inseq = inseq
-        msg = seto_pb2.seq_message()
-        msg.payload.login.username = username
-        msg.payload.login.password = password
+        msg = seto_pb2.payload()
+        msg.sequenced.message_data.login.username = username
+        msg.sequenced.message_data.login.password = password
         if session:
-            msg.payload.login.session = session
+            msg.sequenced.message_data.login.session = session
         self.out.send(msg)
         self.sock = sock
+        self.buffer = ''
         self.start()
+
+    def _read_frame(self):
+        """
+        Read a frame with header
+        """
+        msg = seto_pb2.payload()
+        # Read a minimum of 4 bytes
+        bytes_needed = 4 - len(self.buffer)
+        if bytes_needed:
+            self.buffer += self.sock.recv(bytes_needed, socket.MSG_WAITALL)
+        result = 0
+        shift = 0
+        pos = 0
+        while 1:
+            if pos > len(self.buffer) - 1:
+                pos = 0
+                # Empty buffer and read another 4 bytes
+                self.buffer = self.sock.recv(4, socket.MSG_WAITALL)
+            b = ord(self.buffer[pos])
+            result |= ((b & 0x7f) << shift)
+            pos += 1
+            if not (b & 0x80):
+                self.buffer = self.buffer[pos:]
+                to_read = max(0, result - len(self.buffer))
+                if to_read:
+                    # Read the actual message if necessary
+                    self.buffer += self.sock.recv(to_read, socket.MSG_WAITALL)
+                msg.ParseFromString(self.buffer[:result])
+                # Consume the buffer
+                self.buffer = self.buffer[result:]
+                return msg
+            shift += 7
 
     def run(self):
         while True:
-            header_bytes = self.sock.recv(2)
-            (header,) = struct.unpack('>H', header_bytes)
-            if header&32768:
-                byte_count = header - 32768
-                msg_bytes = self.sock.recv(byte_count)
-                msg = seto_pb2.transient()
-                msg.ParseFromString(msg_bytes)
-                self.handle_transient(msg)
-            else:
-                msg_bytes = self.sock.recv(header)
-                msg = seto_pb2.seq_message()
-                msg.ParseFromString(msg_bytes)
-                if self.pre_handle(msg):
-                    try:
-                        self.handle(msg)
-                        self.inseq += 1
-                    except Exception, e:
-                        print "Error handling message", msg, e
+            msg = self._read_frame()
+            if self.pre_handle(msg):
+                try:
+                    self.handle(msg)
+                    self.inseq += 1
+                except Exception, e:
+                    print "Error handling message", msg, e
 
     def pre_handle(self, msg):
-        if msg.seq == self.inseq:
+        if msg.sequenced.seq == self.inseq:
             return True # correct sequence
-        elif msg.payload.replay.seq:
+        elif msg.sequenced.message_data.replay.seq:
             # replay message, sequence not important, process it here
             return False
-        elif msg.seq > self.inseq:
-            replay = seto_pb2.seq_message()
-            replay.payload.replay.seq = self.inseq
+        elif msg.sequenced.seq > self.inseq:
+            replay = seto_pb2.payload()
+            replay.sequenced.message_data.replay.seq = self.inseq
             self.out.send(replay)
             return False
         else:
             return False
 
     def handle(self, msg):
-        if msg.payload.login_response.session:
-            self.session = msg.payload.login_response.session
-            self.out.seq = msg.payload.login_response.reset
-            print "Session", msg.payload.login_response.session
-        elif msg.payload.order_accepted.order:
-            print "Order Accepted", msg.payload.order_accepted.seq, msg.payload.order_accepted.order
-        elif msg.payload.order_executed.order:
-            print "Order Executed", msg.payload.order_executed.order, \
-                    msg.payload.order_executed.price,\
-                    msg.payload.order_executed.quantity
-        elif msg.payload.order_cancelled.order:
-            print "Order Cancelled", msg.payload.order_cancelled.order, msg.payload.order_cancelled.reason
-        elif msg.payload.pong:
+        if msg.sequenced.message_data.login_response.session:
+            self.session = msg.sequenced.message_data.login_response.session
+            self.out.seq = msg.sequenced.message_data.login_response.reset
+            print "Session", msg.sequenced.message_data.login_response.session
+        elif msg.sequenced.message_data.order_accepted.order:
+            print "Order Accepted", msg.sequenced.message_data.order_accepted.seq, msg.sequenced.message_data.order_accepted.order
+        elif msg.sequenced.message_data.order_executed.order:
+            print "Order Executed", msg.sequenced.message_data.order_executed.order, \
+                    msg.sequenced.message_data.order_executed.price,\
+                    msg.sequenced.message_data.order_executed.quantity
+        elif msg.sequenced.message_data.order_cancelled.order:
+            print "Order Cancelled", msg.sequenced.message_data.order_cancelled.order, msg.sequenced.message_data.order_cancelled.reason
+        elif msg.sequenced.message_data.pong:
             print "Pong"
-        elif msg.payload.market_subscribed.group:
-            print "Subscribed to market", msg.payload.market_subscribed.group
+        elif msg.sequenced.message_data.market_quotes:
+            print "Quotes ", msg.sequenced.message_data.market_quotes.group
 
-    def handle_transient(self, msg):
-        if msg.push_accepted.quantity:
-            payload = msg.push_accepted
-            print "Push Accepted", payload.quantity, payload.price, payload.side
-        elif msg.push_executed.quantity:
-            payload = msg.push_executed
-            print "Push Execution", payload.quantity, payload.price, payload.side
+
+def _encode_varint(value):
+    """
+    Encode an int/long as a ULEB128 number
+    """
+    bits = value & 0x7f
+    value >>= 7
+    ret = ''
+    while value:
+        ret += chr(0x80|bits)
+        bits = value & 0x7f
+        value >>= 7
+    return ret + chr(bits)
