a8e7e0ca72525c14696f7dc626081191c631147f
Hunter Morris <hunter.morris@smarkets.com>
2011-08-23 20:55:07 +0100

    Move build process to setup.py

--- Makefile
+++ Makefile
@@ -2,45 +2,20 @@ NAME = smk_python_sdk
 VERSION = $(shell python setup.py --version)
 DST = dist/$(NAME)-$(VERSION)
 
-all: smk.seto.piqi.proto README
-
-build/pb:
-	mkdir -p build/pb
-	(cd build/pb && \
-		git clone git://git.corp.smarkets.com/smk_api_common.git && \
-		cd smk_api_common && \
-		./rebar get-deps)
-
-eto.piqi: build/pb
-	cp build/pb/smk_api_common/deps/eto_common/eto.piqi .
-
-seto.piqi: build/pb
-	cp build/pb/smk_api_common/seto.piqi .
-
-smk.eto.piqi.proto: eto.piqi
-	piqi to-proto eto.piqi -o smk.eto.piqi.proto
-
-smk.seto.piqi.proto: smk.eto.piqi.proto seto.piqi
-	piqi to-proto seto.piqi -o smk.seto.piqi.proto
-	sed -i.bak 's/import "eto\.piqi\.proto"/import "smk.eto.piqi.proto"/g' \
-		smk.seto.piqi.proto
-	rm -f smk.seto.piqi.proto.bak
-
-# seto/piqi_pb2.py: seto.piqi.proto
-# 	mkdir -p smk/seto
-# 	touch smk/seto/__init__.py
-# 	protoc --python_out=. seto.piqi.proto
-
-# eto/piqi_pb2.py: eto.piqi.proto
-# 	mkdir -p smk/eto
-# 	touch smk/eto/__init__.py
-# 	protoc --python_out=. eto.piqi.proto
+all:
+	python setup.py build
 
 clean:
-	rm -rf eto.piqi seto.piqi smk.seto.piqi.proto smk.eto.piqi.proto smk/eto smk/seto smk/*.pyc build
+	python setup.py clean
 
-README: README.md
-	pandoc -s README.md -w rst -o README
+distclean: clean
+	-rm -rf dist
+	-rm -rf build
+	-rm -rf smk.egg-info
+	-rm -rf smk/eto
+	-rm -rf smk/seto
+	-find . -name "*.pyc" | xargs rm
+	-rm README
 
 release: all
 	-find . -name "*.pyc" | xargs rm
@@ -51,8 +26,8 @@ release: all
 	-rm dist/$(NAME)-$(VERSION).tar.gz
 	-rm dist/$(NAME)-$(VERSION).zip
 	-mkdir dist/$(NAME)-$(VERSION)
-	python setup.py build
-	cp -r smk seto eto tests $(DST)
+	python setup.py sdist --formats=bztar,gztar,zip
+	cp -r smk tests $(DST)
 	cp setup.py README README.md MANIFEST.in CHANGELOG run_tests $(DST)
 	cd dist && tar -czv -f $(NAME)-$(VERSION).tar.gz $(NAME)-$(VERSION)
 	cd dist && zip $(NAME)-$(VERSION).zip -r $(NAME)-$(VERSION)
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import glob
 import os
+import re
+import shutil
 import subprocess
 import sys
 
@@ -13,6 +15,17 @@ except ImportError:
 
 from distutils.spawn import find_executable
 from distutils.command import clean, build
+from itertools import chain
+
+
+COMMON_GIT_REPO = 'git://git.corp.smarkets.com/smk_api_common.git'
+
+
+def _safe_glob(pathname):
+    "Do a safe version of glob which copes with win32"
+    is_win32 = sys.platform == 'win32'
+    for source in glob.glob(pathname):
+        yield source.replace('/', '\\') if is_win32 else source
 
 
 class SmarketsProtocolBuild(build.build):
@@ -20,18 +33,69 @@ class SmarketsProtocolBuild(build.build):
 
     description = "build the protocol buffer output with protobuf-compiler"
 
-    def run(self):
-        "Run the 'protoc' compiler command"
-        protoc = find_executable("protoc")
-        if protoc is None:
+    def check_executables(self):
+        "Check that various executables are available"
+        self.git = find_executable("git")
+        if self.git is None:
+            sys.stderr.write("*** Cannot find git; is it installed?\n")
+            sys.exit(-1)
+
+        self.protoc = find_executable("protoc")
+        if self.protoc is None:
             sys.stderr.write("*** Cannot find protoc; is the protobuf compiler"
                              " installed?\n")
             sys.exit(-1)
 
-        for source in glob.glob('*.proto'):
-            if sys.platform == 'win32':
-                source = source.replace('/', '\\')
-            args = (protoc, '--python_out=.', source)
+        self.piqi = find_executable("piqi")
+        if self.piqi is None:
+            sys.stderr.write("*** Cannot find piqi; are the piqi build tools"
+                             " installed?\n")
+            sys.exit(-1)
+
+    def run(self):
+        "Get the .piqi definitions and run the 'protoc' compiler command"
+        self.check_executables()
+        pb_build_dir = os.path.join(
+            os.path.dirname(__file__), 'build', 'pb')
+        api_dir = os.path.join(pb_build_dir, 'smk_api_common')
+        if not os.path.exists(pb_build_dir):
+            # Get build
+            os.makedirs(pb_build_dir)
+            args = (self.git, 'clone', COMMON_GIT_REPO)
+            if subprocess.call(args, cwd=pb_build_dir) != 0:
+                sys.exit(-1)
+            args = ('./rebar', 'get-deps')
+            if subprocess.call(args, cwd=api_dir) != 0:
+                sys.exit(-1)
+
+        eto_piqi = os.path.join(os.path.dirname(__file__), 'eto.piqi')
+        if not os.path.exists(eto_piqi):
+            eto_piqi_src = os.path.join(
+                api_dir, 'deps', 'eto_common', 'eto.piqi')
+            shutil.copy(eto_piqi_src, eto_piqi)
+
+        seto_piqi = os.path.join(os.path.dirname(__file__), 'seto.piqi')
+        if not os.path.exists(seto_piqi):
+            seto_piqi_src = os.path.join(api_dir, 'seto.piqi')
+            shutil.copy(seto_piqi_src, seto_piqi)
+
+        eto_proto = os.path.join(
+            os.path.dirname(__file__), 'smk.eto.piqi.proto')
+        if not os.path.exists(eto_proto):
+            args = (self.piqi, 'to-proto', eto_piqi, '-o', eto_proto)
+            if subprocess.call(args) != 0:
+                sys.exit(-1)
+
+        seto_proto = os.path.join(
+            os.path.dirname(__file__), 'smk.seto.piqi.proto')
+        if not os.path.exists(seto_proto):
+            args = (self.piqi, 'to-proto', seto_piqi, '-o', seto_proto)
+            if subprocess.call(args) != 0:
+                sys.exit(-1)
+            self.replace_file(seto_proto, self.fix_import)
+
+        for source in _safe_glob('*.proto'):
+            args = (self.protoc, '--python_out=.', source)
             if subprocess.call(args) != 0:
                 sys.exit(-1)
 
@@ -42,6 +106,23 @@ class SmarketsProtocolBuild(build.build):
 
         build.build.run(self)
 
+    @staticmethod
+    def replace_file(filename, line_map):
+        "Map line_map for each line in filename"
+        with open(filename, "r") as sources:
+            lines = sources.readlines()
+        with open(filename, "w") as sources:
+            for line in lines:
+                sources.write(line_map(line))
+
+    @staticmethod
+    def fix_import(line):
+        "Fix the import line in smk.seto.piqi.proto"
+        return re.sub(
+            r'import "eto\.piqi\.proto"',
+            'import "smk.eto.piqi.proto"',
+            line)
+
 
 class SmarketsProtocolClean(clean.clean):
     """Class to clean up the built protobuf files."""
@@ -50,18 +131,37 @@ class SmarketsProtocolClean(clean.clean):
 
     def run(self):
         """Do the clean up"""
-        for source in glob.glob("smk/eto/*.py"):
-            os.unlink(source)
-        for source in glob.glob("smk/seto/*.py"):
-            os.unlink(source)
-        os.unlink('smk/eto')
-        os.unlink('smk/seto')
+        for src_dir in [
+            os.path.join(os.path.dirname(__file__), 'build', 'pb'),
+            os.path.join(os.path.dirname(__file__), 'smk', 'eto'),
+            os.path.join(os.path.dirname(__file__), 'smk', 'seto'),
+            ]:
+            if os.path.exists(src_dir):
+                shutil.rmtree(src_dir)
+        for filename in chain(
+            _safe_glob('*.proto'),
+            _safe_glob('*.piqi')):
+            if os.path.exists(filename):
+                os.unlink(filename)
 
         # Call the parent class clean command
         clean.clean.run(self)
 
 
-f = open(os.path.join(os.path.dirname(__file__), 'README'))
+readme_path = os.path.join(os.path.dirname(__file__), 'README')
+readme_src = os.path.join(os.path.dirname(__file__), 'README.md')
+
+# Generate README from README.md using pandoc
+if not os.path.exists(readme_path):
+    pandoc = find_executable("pandoc")
+    if pandoc is None:
+        sys.stderr.write("*** Cannot find pandoc; is it installed?\n")
+        sys.exit(-1)
+    args = (pandoc, '-s', readme_src, '-w', 'rst', '-o', readme_path)
+    if subprocess.call(args) != 0:
+        sys.exit(-1)
+
+f = open(readme_path)
 long_description = f.read()
 f.close()
 
