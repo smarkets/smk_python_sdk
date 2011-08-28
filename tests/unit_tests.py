db51f5aec6235c1fdd27bb14bc547bba8d3ac3fb
Hunter Morris <hunter.morris@smarkets.com>
2011-08-28 16:33:17 +0100

    Add basic Callback unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,19 +1,9 @@
 import unittest
 
+from itertools import chain
 from mock import Mock
 
 from smarkets.clients import Callback
-
-
-class Handler(object):
-    "Dummy which provides handle methods"
-    def handle_message(self, _msg):
-        "Simple message handler"
-        pass
-
-    def handle_global_message(self, _name, _msg):
-        "Global message handler"
-        pass
 
 
 class CallbackTestCase(unittest.TestCase):
@@ -28,9 +18,65 @@ class CallbackTestCase(unittest.TestCase):
 
     def test_simple_fire(self):
         "Test the simple case where the handler fires"
-        handler = Handler()
-        handler.handle_message = Mock()
-        self.callback += handler.handle_message
+        handler = Mock()
+        self.callback += handler
+        self.assertFalse(handler.called)
+        self.assertEquals(1, len(self.callback))
         self.callback('foo')
-        self.assertTrue(handler.handle_message.called)
-        self.assertEquals(1, handler.handle_message.call_count)
+        handler.assert_called_once_with('foo')
+        self.assertEquals(1, len(self.callback))
+
+    def test_unhandle(self):
+        "Test the case where a handler is removed"
+        handler = Mock()
+        self.callback += handler
+        self.assertFalse(handler.called)
+        self.assertEquals(1, len(self.callback))
+        self.callback -= handler
+        self.assertEquals(0, len(self.callback))
+        self.callback('foo')
+        self.assertFalse(handler.called)
+
+    def test_2_handlers(self):
+        "Test 2 handlers both get called"
+        handler1 = Mock()
+        handler2 = Mock()
+        self.callback += handler1
+        self.callback += handler2
+        self.assertFalse(handler1.called)
+        self.assertFalse(handler2.called)
+        self.assertEquals(2, len(self.callback))
+        self.callback('foo')
+        handler1.assert_called_once_with('foo')
+        handler2.assert_called_once_with('foo')
+        self.assertEquals(2, len(self.callback))
+
+    def test_many_handlers(self):
+        "General version of `test_2_handlers`"
+        handlers = [Mock() for _ in xrange(1, 100)]
+        for handler in handlers:
+            self.callback += handler
+        self.assertEquals(len(handlers), len(self.callback))
+        for handler in handlers:
+            self.assertFalse(handler.called)
+        self.callback('foo')
+        for handler in handlers:
+            handler.assert_called_once_with('foo')
+        self.assertEquals(len(handlers), len(self.callback))
+
+    def test_many_unhandle(self):
+        "Unhandle many"
+        real_handlers = [Mock() for _ in xrange(1, 100)]
+        to_unhandle = [Mock() for _ in xrange(1, 20)]
+        for handler in chain(real_handlers, to_unhandle):
+            self.callback += handler
+        self.assertEquals(
+            len(real_handlers) + len(to_unhandle), len(self.callback))
+        for handler in to_unhandle:
+            self.callback -= handler
+        self.assertEquals(len(real_handlers), len(self.callback))
+        self.callback('foo')
+        for handler in to_unhandle:
+            self.assertFalse(handler.called)
+        for handler in real_handlers:
+            handler.assert_called_once_with('foo')
