cff55b71293e4b2b3907a73aba1e849d6c3314bc
Jakub Stasiak <jakub@stasiak.at>
2013-07-15 17:33:08 +0100

    Bump version

--- smarkets/__init__.py
+++ smarkets/__init__.py
@@ -46,7 +46,7 @@ from smarkets.orders import OrderCreate
 from smarkets.sessions import Session, SessionSettings
 
 
-__version__ = '0.4.7'
+__version__ = '0.4.8'
 
 __all__ = sorted(name for name, obj in locals().items()
                  if not (name.startswith('_') or inspect.ismodule(obj)))
