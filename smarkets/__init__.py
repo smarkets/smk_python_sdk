a1eabcb2a7fba384f351e717d7f4957dd70c5815
Jakub Stasiak <jakub@stasiak.at>
2013-10-14 17:35:27 +0100

    Restore distributing proto files

--- smarkets/__init__.py
+++ smarkets/__init__.py
@@ -12,7 +12,7 @@ if 'READTHEDOCS' in os.environ:
 
     sys.modules['smarkets.streaming_api.eto'] = sys.modules['smarkets.streaming_api.seto'] = Mock()
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 
 def private(something):
