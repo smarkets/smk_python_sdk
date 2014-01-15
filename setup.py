c5ac82aba52cee792a0dc2d7c01bf7bf23887e49
Terence Hoosen <terence.hoosen@smarkets.com>
2014-01-15 15:13:16 +0000

    Reference new seto.piqi for event subscribe and runningball messages. Bump version.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def check_call(*args, **kwargs):
 
 
 ETO_PIQI_URL = 'https://raw.github.com/smarkets/eto_common/v1.0.0/eto.piqi'
-SETO_PIQI_URL = 'https://raw.github.com/smarkets/smk_api_common/v1.0.0/seto.piqi'
+SETO_PIQI_URL = 'https://raw.github.com/smarkets/smk_api_common/v1.0.1/seto.piqi'
 
 
 def _safe_glob(pathname):
