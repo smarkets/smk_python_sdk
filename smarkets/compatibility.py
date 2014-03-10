52a563d7206361e6dbab3601a586f137106ba2d3
Jakub Stasiak <jakub@stasiak.at>
2014-03-10 14:32:29 +0000

    Force every syslog message to fit in single line by default

--- smarkets/compatibility.py
+++ smarkets/compatibility.py
@@ -19,6 +19,10 @@ class UTFFixedSysLogHandler(SysLogHandler):
     Fixes BOM issue, bug Reference: http://bugs.python.org/issue7077
     """
 
+    def __init__(self, join_lines=True, **kwargs):
+        self.join_lines = join_lines
+        SysLogHandler.__init__(self, **kwargs)
+
     def emit(self, record):
         """
         Emit a record.
@@ -26,32 +30,38 @@ class UTFFixedSysLogHandler(SysLogHandler):
         The record is formatted, and then sent to the syslog server.  If
         exception information is present, it is NOT sent to the server.
         """
-        msg = self.format(record) + '\000'
-        """
-        We need to convert record level to lowercase, maybe this will
-        change in the future.
-        """
-        prio = '<%d>' % self.encodePriority(self.facility,
-                                            self.mapPriority(record.levelname))
-        prio = prio.encode('utf-8')
-
-        # Message may be a string. Convert to bytes as required by RFC 5424.
-        if isinstance(msg, text_type):
-            msg = msg.encode('utf-8')
-
-        msg = prio + msg
+        original_message = record.msg
         try:
-            if self.unixsocket:
-                try:
-                    self.socket.send(msg)
-                except socket.error:
-                    self._connect_unixsocket(self.address)
-                    self.socket.send(msg)
-            elif getattr(self, 'socktype', socket.SOCK_DGRAM):
-                self.socket.sendto(msg, self.address)
-            else:
-                self.socket.sendall(msg)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
+            if self.join_lines:
+                record.msg = original_message.replace('\n', r'\n')
+            msg = self.format(record) + '\000'
+            """
+            We need to convert record level to lowercase, maybe this will
+            change in the future.
+            """
+            prio = '<%d>' % self.encodePriority(self.facility,
+                                                self.mapPriority(record.levelname))
+            prio = prio.encode('utf-8')
+
+            # Message may be a string. Convert to bytes as required by RFC 5424.
+            if isinstance(msg, text_type):
+                msg = msg.encode('utf-8')
+
+            msg = prio + msg
+            try:
+                if self.unixsocket:
+                    try:
+                        self.socket.send(msg)
+                    except socket.error:
+                        self._connect_unixsocket(self.address)
+                        self.socket.send(msg)
+                elif getattr(self, 'socktype', socket.SOCK_DGRAM):
+                    self.socket.sendto(msg, self.address)
+                else:
+                    self.socket.sendall(msg)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                self.handleError(record)
+        finally:
+            record.msg = original_message
