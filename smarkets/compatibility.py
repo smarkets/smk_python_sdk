949988a2b8f3bd0a959c53c9bab58ca21aea1898
Jakub Stasiak <jakub@stasiak.at>
2014-03-10 19:02:20 +0000

    Split lines in the actual syslog output, not the format string

--- smarkets/compatibility.py
+++ smarkets/compatibility.py
@@ -30,38 +30,34 @@ class UTFFixedSysLogHandler(SysLogHandler):
         The record is formatted, and then sent to the syslog server.  If
         exception information is present, it is NOT sent to the server.
         """
-        original_message = record.msg
+        msg = self.format(record) + '\000'
+        if self.join_lines:
+            msg = msg.replace('\n', r'\n')
+        """
+        We need to convert record level to lowercase, maybe this will
+        change in the future.
+        """
+        prio = '<%d>' % self.encodePriority(self.facility,
+                                            self.mapPriority(record.levelname))
+        prio = prio.encode('utf-8')
+
+        # Message may be a string. Convert to bytes as required by RFC 5424.
+        if isinstance(msg, text_type):
+            msg = msg.encode('utf-8')
+
+        msg = prio + msg
         try:
-            if self.join_lines:
-                record.msg = original_message.replace('\n', r'\n')
-            msg = self.format(record) + '\000'
-            """
-            We need to convert record level to lowercase, maybe this will
-            change in the future.
-            """
-            prio = '<%d>' % self.encodePriority(self.facility,
-                                                self.mapPriority(record.levelname))
-            prio = prio.encode('utf-8')
-
-            # Message may be a string. Convert to bytes as required by RFC 5424.
-            if isinstance(msg, text_type):
-                msg = msg.encode('utf-8')
-
-            msg = prio + msg
-            try:
-                if self.unixsocket:
-                    try:
-                        self.socket.send(msg)
-                    except socket.error:
-                        self._connect_unixsocket(self.address)
-                        self.socket.send(msg)
-                elif getattr(self, 'socktype', socket.SOCK_DGRAM):
-                    self.socket.sendto(msg, self.address)
-                else:
-                    self.socket.sendall(msg)
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except:
-                self.handleError(record)
-        finally:
-            record.msg = original_message
+            if self.unixsocket:
+                try:
+                    self.socket.send(msg)
+                except socket.error:
+                    self._connect_unixsocket(self.address)
+                    self.socket.send(msg)
+            elif getattr(self, 'socktype', socket.SOCK_DGRAM):
+                self.socket.sendto(msg, self.address)
+            else:
+                self.socket.sendall(msg)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
