8ce19e0472b5019c6e2c9c721e867b985411cc92
Jakub Stasiak <jakub@stasiak.at>
2014-03-17 15:35:39 +0000

    Bump version to 1.1.0c1

--- smarkets/__init__.py
+++ smarkets/__init__.py
@@ -3,7 +3,7 @@
 # This module is released under the MIT License:
 # http://www.opensource.org/licenses/mit-license.php
 
-__version__ = '1.0.13'
+__version__ = '1.1.0c1'
 
 
 def private(something):
