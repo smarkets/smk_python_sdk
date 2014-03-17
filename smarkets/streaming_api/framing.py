0fd6caf7117f93fb145da6dbaa9989e49dc7ff9b
Jakub Stasiak <jakub@stasiak.at>
2014-03-17 12:02:59 +0000

    Refactor frame/uleb128 encoding/decoding into functions

---
+++ smarkets/streaming_api/framing.py
@@ -0,0 +1,95 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+def frame_encode(payload):
+    """
+    :type payload: byte string
+    :rtype: byte string
+    """
+    byte_count = len(payload)
+    header = uleb128_encode(byte_count)
+
+    # Minimum frame size is 4 bytes
+    padding = b'\x00' * max(0, 4 - len(header) - len(payload))
+
+    return header + payload + padding
+
+
+def frame_decode_all(string):
+    """
+    :type string: byte string
+    :rtype: tuple of (list of payloads) and remaining string
+    """
+    payloads = []
+    frame_size = 4
+    while len(string) >= frame_size:
+        try:
+            decoded = uleb128_decode(string)
+        except IncompleteULEB128:
+            # There may be not enough data in the input string to decode the header
+            break
+        else:
+            payload_size, header_size = decoded
+            frame_size = max(payload_size + header_size, 4)
+            if len(string) >= frame_size:
+                frame, string = string[:frame_size], string[frame_size:]
+                payloads.append(frame[header_size:header_size + payload_size])
+
+    return payloads, string
+
+
+def uleb128_encode(value):
+    """Encode a nonnegative int/long as a ULEB128 number.
+
+    :type value: int or long
+    :rtype: byte string
+    """
+    if value < 0:
+        raise ValueError('Value needs to be nonnegative, got %r' % value)
+
+    CONTINUATION_MARK = 0x80
+    LEAST_MEANINGFUL_PART_MASK = 0x7f
+    bits = value & LEAST_MEANINGFUL_PART_MASK
+    value >>= 7
+    ret = b''
+    while value:
+        ret += chr(CONTINUATION_MARK | bits)
+        bits = value & LEAST_MEANINGFUL_PART_MASK
+        value >>= 7
+    return ret + chr(bits)
+
+
+class IncompleteULEB128(Exception):
+    pass
+
+
+def uleb128_decode(string):
+    """
+    :type string: byte string
+    :return: decoded value and number of bytes from the `string` used to decode it
+    :rtype: tuple of (int or long) and int
+    :raises:
+        :IncompleteULEB128: when `string` doesn't start with ULEB128-encoded number.
+    """
+    CONTINUATION_MARK = 0x80
+    LEAST_MEANINGFUL_PART_MASK = 0x7f
+
+    shift = 0
+    result = 0
+    position = 0
+
+    while True:
+        try:
+            current_byte = ord(string[position])
+        except IndexError:
+            raise IncompleteULEB128('String does not start with a ULEB128-encoded value')
+        else:
+            result |= ((current_byte & LEAST_MEANINGFUL_PART_MASK) << shift)
+
+            if not current_byte & CONTINUATION_MARK:
+                break
+            else:
+                shift += 7
+                position += 1
+
+    return result, position + 1
---
+++ smarkets/tests/streaming_api/framing.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from itertools import chain
+
+from nose.tools import eq_, raises
+
+from smarkets.streaming_api.framing import (
+    frame_decode_all, frame_encode, IncompleteULEB128, uleb128_decode, uleb128_encode,
+)
+
+
+test_data = (
+    (0x00000000, b'\x00'),
+    (0x0000007F, b'\x7F'),
+    (0x00000080, b'\x80\x01'),
+    (624485, b'\xE5\x8E\x26'),
+    (268435202, b'\x82\xFE\xFF\x7F'),
+)
+
+
+def test_dumps():
+    for value, string in test_data:
+        yield check_dumps, value, string
+
+
+def check_dumps(value, string):
+    eq_(uleb128_encode(value), string)
+
+
+def test_loads():
+    for value, string in test_data:
+        yield check_loads, string, value
+
+
+def check_loads(string, value):
+    eq_(uleb128_decode(string), (value, len(string)))
+
+
+def test_loads_and_dumps_are_consistent():
+    for i in chain(
+        xrange(2 ** 20),
+        xrange(2 ** 20, 2 ** 26, 3333),
+        xrange(2 ** 26, 2 ** 32, 77777),
+    ):
+        byte_dump = uleb128_encode(i)
+        eq_(uleb128_decode(byte_dump), (i, len(byte_dump)))
+
+
+@raises(ValueError)
+def test_uleb128_encode_fails_on_negative_number():
+    uleb128_encode(-1)
+
+
+def test_uleb128_decode_fails_on_invalid_input():
+    data = uleb128_encode(12345678)
+
+    for i in xrange(len(data)):
+        yield check_uleb128_decode_fails_on_invalid_input, data[:i]
+
+
+@raises(IncompleteULEB128)
+def check_uleb128_decode_fails_on_invalid_input(input_):
+    uleb128_decode(input_)
+
+
+def test_frame_encode():
+    for input_, output in (
+        (b'', b'\x00\x00\x00\x00'),
+        (b'a', b'\x01a\x00\x00'),
+        (b'ab', b'\x02ab\x00'),
+        (b'abc', b'\x03abc'),
+        (b'abcd', b'\x04abcd'),
+    ):
+        yield check_frame_encode, input_, output
+
+
+def check_frame_encode(input_, output):
+    eq_(frame_encode(input_), output)
+
+
+def test_frame_decode_all():
+    for input_, output in (
+        # frame matches the boundary
+        (b'', ([], b'')),
+        (b'\x01a\x00\x00\x02ab\x00\x03abc\x04abcd', ([b'a', b'ab', b'abc', b'abcd'], b'')),
+
+        # ends with complete header but only part of a message
+        (b'\x03ab', ([], b'\x03ab')),
+        (b'\x01a\x00\x00\x02ab\x00\x03abc\x04abcd\x03ab', ([b'a', b'ab', b'abc', b'abcd'], b'\x03ab')),
+
+        # ends with incomplete header
+        (b'\x80', ([], b'\x80')),
+        (b'\x01a\x00\x00\x02ab\x00\x03abc\x04abcd\x03ab', ([b'a', b'ab', b'abc', b'abcd'], b'\x03ab')),
+
+        # 4(or more)-byte incomplete header is a special case because it reaches the minimum frame size
+        # so let's make sure decoding doesn't fail at header decoding stage
+        (b'\x80\x80\x80\x80', ([], b'\x80\x80\x80\x80')),
+        (b'\x80\x80\x80\x80\x80', ([], b'\x80\x80\x80\x80\x80')),
+    ):
+        yield check_frame_decode_all, input_, output
+
+
+def check_frame_decode_all(input_, output):
+    eq_(frame_decode_all(input_), output)
