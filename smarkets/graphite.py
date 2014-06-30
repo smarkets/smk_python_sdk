5ad19b4f4bb3e78732f3f5e17acd5976e4b94b83
Jakub Stasiak <jakub@stasiak.at>
2014-06-30 16:35:35 +0100

    Add UDP socket option to Graphite client

--- smarkets/graphite.py
+++ smarkets/graphite.py
@@ -1,18 +1,22 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging
 import time
 import socket
+
+log = logging.getLogger(__name__)
 
 
 class Graphite(object):
 
-    def __init__(self, host='localhost', port=2003, enabled=True, prefix=''):
+    def __init__(self, host='localhost', port=2003, enabled=True, prefix='', use_udp=False):
         self.addr = None
         self.enabled = enabled
         if enabled:
             self.set_address(host, port)
         self.prefix = prefix
         self._socket = None
+        self._use_udp = use_udp
 
     def set_address(self, host, port):
         try:
@@ -34,14 +38,25 @@ class Graphite(object):
         if timestamp is None:
             timestamp = time.time()
         if self._socket is None:
-            self._connect()
-            if self._socket is None:
-                return
+            if self._use_udp:
+                self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            else:
+                self._connect()
+                if self._socket is None:
+                    return
         if self.prefix:
             metric = "%s.%s" % (self.prefix, metric)
+
+        if self._use_udp:
+            send_fun = lambda message: self._socket.sendto(message, self.addr)
+        else:
+            send_fun = self._socket.send
+
         try:
-            self._socket.send("%s %s %s\n" % (metric, value, timestamp))
-        except socket.error:
+            message = "%s %s %s\n" % (metric, value, timestamp)
+            send_fun(message)
+        except socket.error as e:
+            log.warn('Cannot send stuff to Graphite: %r', e)
             self._socket = None
 
 
