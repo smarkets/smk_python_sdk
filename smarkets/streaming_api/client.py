dfd2a7b5cff910a480dab4b0744143d8bab326f6
Jakub Stasiak <jakub@stasiak.at>
2014-07-31 10:53:16 +0100

    Remove deprecated StreamingAPIClient methods

--- smarkets/streaming_api/client.py
+++ smarkets/streaming_api/client.py
@@ -6,7 +6,6 @@
 import logging
 import sys
 
-from smarkets.functools import deprecated_with_message
 from smarkets.signal import Signal
 from smarkets.streaming_api import eto
 from smarkets.streaming_api import seto
@@ -123,57 +122,6 @@ class StreamingAPIClient(object):
         msg.eto_payload.type = eto.PAYLOAD_PING
         self._send()
 
-    @deprecated_with_message('Create appropriate protobuf object and send it using send method')
-    def subscribe(self, market):
-        "Subscribe to a market"
-        msg = self.session.out_payload
-        msg.Clear()
-        msg.type = seto.PAYLOAD_MARKET_SUBSCRIBE
-        msg.market_subscribe.market.CopyFrom(market)
-        self._send()
-
-    @deprecated_with_message('Create appropriate protobuf object and send it using send method')
-    def request_account_state(self):
-        "Request Account State"
-        msg = self.session.out_payload
-        msg.Clear()
-        msg.type = seto.PAYLOAD_ACCOUNT_STATE_REQUEST
-        self._send()
-
-    @deprecated_with_message('Create appropriate protobuf object and send it using send method')
-    def request_orders_for_account(self):
-        "Request an account's orders"
-        msg = self.session.out_payload
-        msg.Clear()
-        msg.type = seto.PAYLOAD_ORDERS_FOR_ACCOUNT_REQUEST
-        self._send()
-
-    @deprecated_with_message('Create appropriate protobuf object and send it using send method')
-    def request_orders_for_market(self, market):
-        "Request an account's orders for a market"
-        msg = self.session.out_payload
-        msg.Clear()
-        msg.type = seto.PAYLOAD_ORDERS_FOR_MARKET_REQUEST
-        msg.orders_for_market_request.market.CopyFrom(market)
-        self._send()
-
-    @deprecated_with_message('Create appropriate protobuf object and send it using send method')
-    def unsubscribe(self, market):
-        "Unsubscribe from a market"
-        msg = self.session.out_payload
-        msg.Clear()
-        msg.type = seto.PAYLOAD_MARKET_UNSUBSCRIBE
-        msg.market_unsubscribe.market.CopyFrom(market)
-        self._send()
-
-    @deprecated_with_message('Create appropriate protobuf object and send it using send method')
-    def request_events(self, request):
-        "Send a structured events request"
-        msg = self.session.out_payload
-        msg.Clear()
-        request.copy_to(msg)
-        self._send()
-
     def add_handler(self, name, callback):
         "Add a callback handler"
         if not hasattr(callback, '__call__'):
@@ -197,24 +145,6 @@ class StreamingAPIClient(object):
     def del_global_handler(self, callback):
         "Remove a global callback handler"
         self.global_callback -= callback
-
-    @staticmethod
-    @deprecated_with_message('Stop using UUID-s')
-    def str_to_uuid128(uuid_str, uuid128=None, strip_tag=True):
-        "Convert a string to a uuid128"
-        if uuid128 is None:
-            uuid128 = seto.Uuid128()
-        uuid128.Clear()
-        if strip_tag:
-            uuid_str = uuid_str[:-4]
-        low = int(uuid_str[-16:], 16)
-        uuid128.low = low
-        high = uuid_str[:-16]
-        if high:
-            high = int(high, 16)
-            if high:
-                uuid128.high = high
-        return uuid128
 
     def _send(self):
         """
