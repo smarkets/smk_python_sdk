dff6c7f76d203c53a89725f1e01eb2189de2eb27
Lefteris Kritikos <lefteris.kritikos@smarkets.com>
2014-10-22 16:36:25 +0100

    Fix PY3 smarkets/tests/lazy.py

--- smarkets/tests/lazy.py
+++ smarkets/tests/lazy.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from nose.tools import eq_
+from six.moves import xrange
 
 from smarkets.lazy import LazyCall
 
