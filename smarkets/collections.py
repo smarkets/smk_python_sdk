fe47b804f71c5eb67939411749c9569a413057e9
Jakub Stasiak <jakub@stasiak.at>
2014-10-23 13:16:58 +0100

    Fix namedtuple doctest
    The u prefix was there only because of unicode_literals at the top of
    the file and it interacting with builtin exec

--- smarkets/collections.py
+++ smarkets/collections.py
@@ -74,7 +74,7 @@ if PY2:
 
         >>> Point = namedtuple('Point', ['x', 'y'])
         >>> Point.__doc__                   # docstring for the new class
-        u'Point(x, y)'
+        'Point(x, y)'
         >>> p = Point(11, y=22)             # instantiate with positional args or keywords
         >>> p[0] + p[1]                     # indexable like a plain tuple
         33
