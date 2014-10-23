c0718365775ad0189808c9d7eeb4a9c5bc930a67
Jakub Stasiak <jakub@stasiak.at>
2014-10-23 13:56:50 +0100

    Deprecate int_to_uuid128

--- smarkets/streaming_api/utils.py
+++ smarkets/streaming_api/utils.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from smarkets.functools import deprecated_with_message
 from smarkets.streaming_api import seto
 from smarkets.string import camel_case_to_underscores
 
@@ -13,5 +14,6 @@ def set_payload_message(payload, message):
     getattr(payload, underscore_form).CopyFrom(message)
 
 
+@deprecated_with_message('Using SETO UUID128 fields is deprecated now')
 def int_to_uuid128(value):
     return seto.Uuid128(low=value)
