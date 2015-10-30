smarkets/smk_python_sdk

commit 7569527e06369c410dadcca6b3cd17ec65fb8a83

Get rid of iso8601_to_datetime function
parse_datetime does the job just as well, is more strict regarding
errors and returns timezone-native datetime objects by default.

smarkets/datetime.py
@@ -10,7 +10,6 @@ import pytz
 __all__ = (
     'rfc2822_to_datetime',
 
-    'iso8601_to_datetime',
     'datetime_to_iso8601',
     'parse_datetime',
 
@@ -29,32 +28,14 @@ def rfc2822_to_datetime(rfc_date):
     return raw_dt.replace(tzinfo=pytz.utc)
 
 
-def iso8601_to_datetime(iso_date, naive=False):
-    """Converts an ISO-8601 date string to a Python datetime.
-
-    Usage: >>> iso8601_to_datetime('2009-04-24T15:48:26,483772Z')
-           datetime.datetime(2009, 04, 24, 15, 48, 26, 483772)
-    """
-    if iso_date is None:
-        return None
-    iso_date = iso_date.replace(',', '.')
-    try:
-        new_date = iso8601.parse_date(iso_date).astimezone(pytz.utc)
-        if naive:
-            new_date = new_date.replace(tzinfo=None)
-    except iso8601.ParseError:
-        new_date = None
-    return new_date
-
-
 def parse_datetime(iso):
     """Convert ISO8601 formatted timestamp to timezone-naive, UTC-normalized datetime object."""
     return iso8601.parse_date(iso).astimezone(pytz.utc).replace(tzinfo=None)
 
 
 def iso8601_to_date(iso_date):
-    """As above, but just returns a date object"""
-    return iso8601_to_datetime(iso_date + 'T00:00:00').date()
+    """Parse a string representation of a data and return a date object"""
+    return parse_datetime(iso_date + 'T00:00:00').date()
 
 
 def date_to_iso8601(date):

smarkets/tests/datetime.py
@@ -4,22 +4,22 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import datetime
 import unittest
 
-import pytz
-from nose.tools import eq_
+import iso8601
+from nose.tools import assert_raises, eq_
 
-from smarkets.datetime import iso8601_to_datetime
+from smarkets.datetime import parse_datetime
 
 
 class DateTests(unittest.TestCase):
 
     def test_valid_iso_conversion(self):
         valid_iso_dates = (
-            ('2009-04-24T15:48:26,000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0, pytz.utc)),
-            ('2009-04-24T15:48:26', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0, pytz.utc)),
-            ('2009-04-24T15:48:26.000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0, pytz.utc)),
+            ('2009-04-24T15:48:26,000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
+            ('2009-04-24T15:48:26', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
+            ('2009-04-24T15:48:26.000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
         )
         for iso, real_date in valid_iso_dates:
-            result = iso8601_to_datetime(iso)
+            result = parse_datetime(iso)
             eq_(result, real_date, "Date %s didn't match %s" % (result, real_date))
 
     def test_invalid_iso_conversion(self):
@@ -27,5 +27,5 @@ class DateTests(unittest.TestCase):
             'gibberish',
         )
         for iso in invalid_iso_dates:
-            result = iso8601_to_datetime(iso)
-            eq_(result, None, "Date %s didn't fail" % iso)
+            with assert_raises(iso8601.ParseError):
+                parse_datetime(iso)

commit 22d0c1ca306ff011c9d52d723c6f7a7da7135bdd

Bring back support for commas as ISO 8601 separators
The standard does allow that and since we want parse_datetime to
completely replace iso8601_to_datetime let's keep supporting it.

Fixes: 7569527e06369c410dadcca6b3cd17ec65fb8a83

smarkets/datetime.py
@@ -30,6 +30,7 @@ def rfc2822_to_datetime(rfc_date):
 
 def parse_datetime(iso):
     """Convert ISO8601 formatted timestamp to timezone-naive, UTC-normalized datetime object."""
+    iso = iso.replace(',', '.')
     return iso8601.parse_date(iso).astimezone(pytz.utc).replace(tzinfo=None)
 
 

smarkets/tests/datetime.py
@@ -14,7 +14,7 @@ class DateTests(unittest.TestCase):
 
     def test_valid_iso_conversion(self):
         valid_iso_dates = (
-            ('2009-04-24T15:48:26,000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
+            ('2009-04-24T15:48:26.000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
             ('2009-04-24T15:48:26', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
             ('2009-04-24T15:48:26.000000Z', datetime.datetime(2009, 0o4, 24, 15, 48, 26, 0)),
         )