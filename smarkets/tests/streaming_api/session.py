smarkets/smk_python_sdk

commit e4aebdbf5ba56223e39ac0fdf6b58d526f11c524

Use bytearray for buffers instead of byte strings
Python strings are immutable. Thus, using += operator
on them, creates a copy and leads to quadratic time
complexity.

To measure it, I wrote a small benchmark reflecting
the code in session.py:

```
import time

def timeit(method):

    def timed(*args, **kw):
        ts = time.time()
        result = method(*args, **kw)
        te = time.time()
        print '%r %2.2f sec' % \
              (method.__name__, te - ts)
        return result

    return timed

ITERATIONS = 1000000
CHUNK = 'a' * 10000

@timeit
def concat():
	send_buffer = ''
	for i in range(ITERATIONS):
		send_buffer += CHUNK

@timeit
def byte_array():
	send_buffer = bytearray()
	for i in range(ITERATIONS):
		send_buffer += CHUNK

byte_array()
concat()
```

I run it 3 times like that and 3 times in different order:

Results:

adam@adam-box:~/Desktop$ python test.py
'concat' 3.26 sec
'byte_array' 1.39 sec
adam@adam-box:~/Desktop$ python test.py
'concat' 3.30 sec
'byte_array' 1.34 sec
adam@adam-box:~/Desktop$ python test.py
'concat' 3.24 sec
'byte_array' 1.40 sec

adam@adam-box:~/Desktop$ python test.py
'byte_array' 1.55 sec
'concat' 3.16 sec
adam@adam-box:~/Desktop$ python test.py
'byte_array' 1.54 sec
'concat' 3.20 sec
adam@adam-box:~/Desktop$ python test.py
'byte_array' 1.60 sec
'concat' 3.19 sec

'concat' average over 6 runs: 3.26 sec
'byte_array' average over 6 runs: 1.46 sec

'byte_array' on average was 2.2x faster.

Tests were for appending a chunk of length 10k,
which can happen (it can be even worse) during spikes.

Additionally, using strings creates a lot of temporary
string objects, which later GC has to collect, which
hits performance even more.

This commit uses mutable bytearrays in place where
immutable strings where concatenated, reducing time
complexity of appending from quadratic to linear.

Specifically, it changes send_buffer and read_buffer
variable types from byte strings to bytearrays.

smarkets/streaming_api/session.py
@@ -57,8 +57,8 @@ class Session(object):
         # Outgoing buffer sequence number
         self.buf_outseq = outseq
         self.out_payload = seto.Payload()
-        self.send_buffer = b''
-        self.read_buffer = b''
+        self.send_buffer = bytearray()
+        self.read_buffer = bytearray()
         self.buffered_incoming_payloads = []
 
     @property
@@ -105,7 +105,7 @@ class Session(object):
                 'Clearing non-empty buffer, %d bytes will be lost: %r',
                 len(self.send_buffer), self.send_buffer,
             )
-        self.send_buffer = b''
+        self.send_buffer = bytearray()
 
     def logout(self):
         "Disconnects from the API"
@@ -129,7 +129,7 @@ class Session(object):
             LazyCall(MessageToString, self.out_payload))
         sent_seq = self.buf_outseq
         self.out_payload.eto_payload.seq = sent_seq
-        self.send_buffer += frame_encode(self.out_payload.SerializeToString())
+        frame_encode(self.send_buffer, self.out_payload.SerializeToString())
         self.buf_outseq += 1
 
     def flush(self):
@@ -138,7 +138,7 @@ class Session(object):
         if self.send_buffer:
             bytes_sent = self.socket.send(self.send_buffer)
             self.flush_logger.debug("Flushed %d bytes out of %d", bytes_sent, len(self.send_buffer))
-            self.send_buffer = self.send_buffer[bytes_sent:]
+            self.send_buffer[0:] = self.send_buffer[bytes_sent:]
 
     def read(self):
         self.read_buffer += self.socket.recv()
@@ -260,17 +260,17 @@ class SessionSocket(object):
             self.logger.debug('Exception when disconnecting: %r', e)
         self._sock = None
 
-    def send(self, data):
+    def send(self, byte_array):
         """
-        :type data: byte string
+        :type byte_array: bytearray
         :return: Number of sent bytes
         :rtype: int
         """
         if self._sock is None:
             raise SocketDisconnected('Trying to write to socket when disconnected')
         try:
-            self.wire_logger.debug("sending %d bytes: %r", len(data), data)
-            sent = self._sock.send(data)
+            self.wire_logger.debug("sending %d bytes: %r", len(byte_array), byte_array)
+            sent = self._sock.send(byte_array)
             if sent == 0:
                 raise SocketDisconnected('Socket disconnected when writing to it, 0 bytes written')
             return sent

smarkets/streaming_api/framing.py
@@ -1,79 +1,82 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from six import indexbytes, int2byte
+from six import int2byte
 
 
-def frame_encode(payload):
-    """
-    :type payload: byte string
-    :rtype: byte string
-    """
-    byte_count = len(payload)
-    header = uleb128_encode(byte_count)
-
-    # Minimum frame size is 4 bytes
-    padding = b'\x00' * max(0, 4 - len(header) - len(payload))
-
-    return header + payload + padding
-
-
-def frame_decode_all(string):
-    """
-    :type string: byte string
-    :rtype: tuple of (list of payloads) and remaining string
-    """
-    payloads = []
-    min_frame_size = 4
-    while len(string) >= min_frame_size:
-        try:
-            decoded = uleb128_decode(string)
-        except IncompleteULEB128:
-            # There may be not enough data in the input string to decode the header
-            break
-        else:
-            payload_size, header_size = decoded
-            frame_size = max(payload_size + header_size, min_frame_size)
-            if len(string) >= frame_size:
-                frame, string = string[:frame_size], string[frame_size:]
-                payloads.append(frame[header_size:header_size + payload_size])
-            else:
-                break
-
-    return payloads, string
-
-
-def uleb128_encode(value):
-    """Encode a nonnegative int/long as a ULEB128 number.
-
-    :type value: int or long
-    :rtype: byte string
-    """
-    if value < 0:
-        raise ValueError('Value needs to be nonnegative, got %r' % value)
-
-    CONTINUATION_MARK = 0x80
-    LEAST_MEANINGFUL_PART_MASK = 0x7f
-    bits = value & LEAST_MEANINGFUL_PART_MASK
-    value >>= 7
-    ret = b''
-    while value:
-        ret += int2byte(CONTINUATION_MARK | bits)
-        bits = value & LEAST_MEANINGFUL_PART_MASK
-        value >>= 7
-    return ret + int2byte(bits)
+MIN_FRAME_SIZE = 4
 
 
 class IncompleteULEB128(Exception):
     pass
 
 
-def uleb128_decode(string):
+def frame_encode(frame, payload):
     """
-    :type string: byte string
-    :return: decoded value and number of bytes from the `string` used to decode it
+    Encodes payload and appends it to the given frame.
+    :type frame: bytearray
+    :type payload: byte string or bytearray
+    """
+    byte_count = len(payload)
+    header = uleb128_encode(byte_count)
+    padding = b'\x00' * max(0, MIN_FRAME_SIZE - len(header) - byte_count)
+    frame += header
+    frame += payload
+    frame += padding
+
+
+def uleb128_encode(value):
+    """Encode a non-negative int/long as a ULEB128 number.
+
+    :type value: int or long
+    :rtype: bytearray
+    """
+    if value < 0:
+        raise ValueError('Value needs to be non-negative, got %r' % value)
+
+    CONTINUATION_MARK = 0x80
+    LEAST_MEANINGFUL_PART_MASK = 0x7f
+    bits = value & LEAST_MEANINGFUL_PART_MASK
+    value >>= 7
+    ret = bytearray()
+    while value:
+        ret += int2byte(CONTINUATION_MARK | bits)
+        bits = value & LEAST_MEANINGFUL_PART_MASK
+        value >>= 7
+    ret += int2byte(bits)
+    return ret
+
+
+def frame_decode_all(to_decode):
+    """
+    :type to_decode: bytes
+    :rtype: tuple (list of bytes, remaining bytes)
+    """
+    payloads = []
+    while len(to_decode) >= MIN_FRAME_SIZE:
+        try:
+            decoded = uleb128_decode(to_decode)
+        except IncompleteULEB128:
+            # There may be not enough data in the input to decode the header
+            break
+        else:
+            payload_size, header_size = decoded
+            frame_size = max(payload_size + header_size, MIN_FRAME_SIZE)
+            if len(to_decode) >= frame_size:
+                frame, to_decode = to_decode[:frame_size], to_decode[frame_size:]
+                payloads.append(frame[header_size:header_size + payload_size])
+            else:
+                break
+
+    return payloads, to_decode
+
+
+def uleb128_decode(to_decode):
+    """
+    :type to_decode: bytes
+    :return: decoded value and number of bytes from `to_decode` used to decode it
     :rtype: tuple of (int or long) and int
     :raises:
-        :IncompleteULEB128: when `string` doesn't start with ULEB128-encoded number.
+        :IncompleteULEB128: when `to_decode` doesn't start with ULEB128-encoded number.
     """
     CONTINUATION_MARK = 0x80
     LEAST_MEANINGFUL_PART_MASK = 0x7f
@@ -84,9 +87,9 @@ def uleb128_decode(string):
 
     while True:
         try:
-            current_byte = indexbytes(string, position)
+            current_byte = to_decode[position]
         except IndexError:
-            raise IncompleteULEB128('String does not start with a ULEB128-encoded value')
+            raise IncompleteULEB128('to_decode does not start with a ULEB128-encoded value')
         else:
             result |= ((current_byte & LEAST_MEANINGFUL_PART_MASK) << shift)
 

smarkets/tests/streaming_api/framing.py
@@ -30,11 +30,11 @@ def check_dumps(value, string):
 
 def test_loads():
     for value, string in test_data:
-        yield check_loads, string, value
+        yield check_loads, bytearray(string), value
 
 
-def check_loads(string, value):
-    eq_(uleb128_decode(string), (value, len(string)))
+def check_loads(byte_array, value):
+    eq_(uleb128_decode(byte_array), (value, len(byte_array)))
 
 
 def test_loads_and_dumps_are_consistent():
@@ -53,10 +53,10 @@ def test_uleb128_encode_fails_on_negative_number():
 
 
 def test_uleb128_decode_fails_on_invalid_input():
-    data = uleb128_encode(12345678)
+    byte_array = uleb128_encode(12345678)
 
-    for i in xrange(len(data)):
-        yield check_uleb128_decode_fails_on_invalid_input, data[:i]
+    for i in xrange(len(byte_array)):
+        yield check_uleb128_decode_fails_on_invalid_input, byte_array[:i]
 
 
 @raises(IncompleteULEB128)
@@ -72,11 +72,13 @@ def test_frame_encode():
         (b'abc', b'\x03abc'),
         (b'abcd', b'\x04abcd'),
     ):
-        yield check_frame_encode, input_, output
+        yield check_frame_encode, bytearray(input_), output
 
 
-def check_frame_encode(input_, output):
-    eq_(frame_encode(input_), output)
+def check_frame_encode(byte_array, output):
+    frame = bytearray()
+    frame_encode(frame, byte_array)
+    eq_(frame, output)
 
 
 def test_frame_decode_all():
@@ -102,8 +104,8 @@ def test_frame_decode_all():
         # regression: if the second frame is shorter, we still want to decode both...
         (b'\x05abcde\x03abc', ([b'abcde', b'abc'], b'')),
     ):
-        yield check_frame_decode_all, input_, output
+        yield check_frame_decode_all, bytearray(input_), output
 
 
-def check_frame_decode_all(input_, output):
-    eq_(frame_decode_all(input_), output)
+def check_frame_decode_all(byte_array, output):
+    eq_(frame_decode_all(byte_array), output)

commit b0626b2e97f933e6f2616881d475e9a0ca7542f6

ParseFromString expects string not bytearray
Fixes bug introduced in e4aebdb.

Added regression test, before it was failing with:

Traceback (most recent call last):
  File "/usr/lib/python2.7/dist-packages/nose/case.py", line 197, in runTest
    self.test(*self.arg)
  File "/home/adam/workspace/smk_python_sdk/smarkets/tests/streaming_api/session.py", line 12, in test_next_frame_regression
    payload = session.next_frame()
  File "/home/adam/workspace/smk_python_sdk/smarkets/streaming_api/session.py", line 168, in next_frame
    payload.ParseFromString((data))
  File "/usr/local/lib/python2.7/dist-packages/google/protobuf/message.py", line 185, in ParseFromString
    self.MergeFromString(serialized)
  File "/usr/local/lib/python2.7/dist-packages/google/protobuf/internal/python_message.py", line 1093, in MergeFromString
    raise message_mod.DecodeError('Truncated message.')
DecodeError: Truncated message.

smarkets/streaming_api/session.py
@@ -165,7 +165,7 @@ class Session(object):
             self.buffered_incoming_payloads[0], self.buffered_incoming_payloads[1:])
 
         payload = seto.Payload()
-        payload.ParseFromString(data)
+        payload.ParseFromString(bytes(data))
         self._handle_in_payload(payload)
         if payload.eto_payload.seq == self.inseq:
             # Go ahead

smarkets/tests/streaming_api/session.py
@@ -1,0 +1,18 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from nose.tools import eq_
+from smarkets.streaming_api.session import SessionSettings, Session
+
+
+def test_next_frame_regression():
+    settings = SessionSettings('username', 'password')
+    session = Session(settings)
+    session_string = '8zysBBGAD6nb95JDIO'
+    session.buffered_incoming_payloads = \
+        [bytearray(
+            b'\x08\x01\x12\x1e\x08\x01\x10\x08\x18\x002\x16\n\x12' +
+            session_string.encode('utf-8') +
+            b'\x10\x02'
+        )]
+    payload = session.next_frame()
+    eq_(payload.eto_payload.login_response.session, session_string)