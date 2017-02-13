091f18877af73c71cfcd801621a4aa01e54c2c4c
Robin Sonefors <robin.sonefors@smarkets.com>
2017-02-13 15:52:39 +0000

    Remove uuid from streaming api, documentation
    We don't use uuids anymore.
    
    Uuid support is still available for internal smarkets use.
    
    Signed-off-by: Robin Sonefors <robin.sonefors@smarkets.com>
    Reviewed-by: Piotr

--- smarkets/streaming_api/api.py
+++ smarkets/streaming_api/api.py
@@ -4,7 +4,6 @@ from smarkets.streaming_api.exceptions import (  # noqa
     ConnectionError, DecodeError, ParseError, SocketDisconnected, InvalidCallbackError)
 from smarkets.streaming_api.session import Session, SessionSettings  # noqa
 from smarkets.streaming_api.seto import IMMEDIATE_OR_CANCEL, GOOD_TIL_CANCELLED  # noqa
-from smarkets.streaming_api.utils import int_to_uuid128  # noqa
 
 SIDE_BID = seto.SIDE_BUY
 SIDE_OFFER = seto.SIDE_SELL
